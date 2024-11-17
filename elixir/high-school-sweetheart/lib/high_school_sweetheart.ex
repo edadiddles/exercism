658fb8f95b12e1a41bfba13af66b2a28eeb40e3d
Edward Ruzek <edward.ruzek@gmail.com>
2024-11-17 12:35:54 -0500

    completed high-school-sweetheart exercise

--- elixir/high-school-sweetheart/lib/high_school_sweetheart.ex
+++ elixir/high-school-sweetheart/lib/high_school_sweetheart.ex
@@ -1,14 +1,17 @@
 defmodule HighSchoolSweetheart do
   def first_letter(name) do
-    # Please implement the first_letter/1 function
+    name |> String.trim |> String.first
   end
 
   def initial(name) do
-    # Please implement the initial/1 function
+    name |> first_letter |> String.upcase |> Kernel.<>(".")
   end
 
   def initials(full_name) do
-    # Please implement the initials/1 function
+    [first, last] = full_name |> String.split
+    first_initial = first |> initial
+    last_initial = last |> initial
+    first_initial <> " " <> last_initial
   end
 
   def pair(full_name1, full_name2) do
@@ -27,6 +30,21 @@ defmodule HighSchoolSweetheart do
     #              ***
     #               *
 
-    # Please implement the pair/2 function
+    
+    "" |>
+    Kernel.<>("     ******       ******\n") |>
+    Kernel.<>("   **      **   **      **\n") |>
+    Kernel.<>(" **         ** **         **\n") |>
+    Kernel.<>("**            *            **\n") |>
+    Kernel.<>("**                         **\n") |>
+    Kernel.<>("**     ") |> Kernel.<>(initials(full_name1)) |> Kernel.<>("  +  ") |> Kernel.<>(initials(full_name2)) |> Kernel.<>("     **\n") |>
+    Kernel.<>(" **                       **\n") |>
+    Kernel.<>("   **                   **\n") |>
+    Kernel.<>("     **               **\n") |>
+    Kernel.<>("       **           **\n") |>
+    Kernel.<>("         **       **\n") |>
+    Kernel.<>("           **   **\n") |>
+    Kernel.<>("             ***\n") |>
+    Kernel.<>("              *\n")
   end
 end
