e22201ef966e1f3726049a3420acf49d0499da93
Edward Ruzek <edward.ruzek@gmail.com>
2024-11-18 07:51:20 -0500

    completed take-a-number exercise

--- elixir/take-a-number/lib/take_a_number.ex
+++ elixir/take-a-number/lib/take_a_number.ex
@@ -1,5 +1,29 @@
 defmodule TakeANumber do
   def start() do
-    # Please implement the start/0 function
+    spawn( fn -> 
+      state = 0
+      loop(state)
+    end)
+  end
+
+  defp loop(state) do
+
+    receive do
+      {:report_state, sender_pid} -> loop( report_state(state, sender_pid) )
+      {:take_a_number, sender_pid} -> loop( take_a_number(state, sender_pid) )
+      :stop -> "stopping maching"
+      _ -> loop(state) 
+    end
+
+  end
+
+  defp report_state(state, pid) do
+    send(pid, state)
+    state
+  end
+
+  defp take_a_number(state, pid) do
+    send(pid, state+1)
+    state+1
   end
 end
