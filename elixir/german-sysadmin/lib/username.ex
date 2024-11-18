368c5daef7cef8f633e55bdba3bdfb8f0de939ee
Edward Ruzek <edward.ruzek@gmail.com>
2024-11-17 19:25:54 -0500

    completed german-sysadmin exercise

--- elixir/german-sysadmin/lib/username.ex
+++ elixir/german-sysadmin/lib/username.ex
@@ -1,10 +1,24 @@
 defmodule Username do
-  def sanitize(username) do
-    # ä becomes ae
-    # ö becomes oe
-    # ü becomes ue
-    # ß becomes ss
 
-    # Please implement the sanitize/1 function
+  def sanitize([]), do: []
+  def sanitize(username) do    
+    [first | rest] = username
+
+    case first do
+      # lower case letters a-z
+      first when first >= 97 and first <= 122 -> [first] ++ sanitize(rest)
+      # ä becomes ae
+      228 -> ~c"ae" ++ sanitize(rest)
+      # ö becomes oe
+      246 -> ~c"oe" ++ sanitize(rest)
+      # ü becomes ue
+      252 -> ~c"ue" ++ sanitize(rest)
+      # ß becomes ss
+      223 -> ~c"ss" ++ sanitize(rest)
+      # allow underscores
+      95 -> [first] ++ sanitize(rest)
+      _ -> [] ++ sanitize(rest)
+    end
+    
   end
 end
