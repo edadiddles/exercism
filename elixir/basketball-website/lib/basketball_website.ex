24e2c20246299b4ff5044b0c5f41187f9658ed9a
Edward Ruzek <edward.ruzek@gmail.com>
2024-11-21 07:53:20 -0500

    completed basketball-website exercise for elixir

--- elixir/basketball-website/lib/basketball_website.ex
+++ elixir/basketball-website/lib/basketball_website.ex
@@ -1,9 +1,11 @@
 defmodule BasketballWebsite do
+  def extract_from_path(data, ""), do: data
   def extract_from_path(data, path) do
-    # Please implement the extract_from_path/2 function
+    [k | tail] = String.split(path, ".", [parts: 2])
+    extract_from_path(data[k], List.last(tail, ""))
   end
 
   def get_in_path(data, path) do
-    # Please implement the get_in_path/2 function
+    Kernel.get_in(data, String.split(path, "."))
   end
 end
