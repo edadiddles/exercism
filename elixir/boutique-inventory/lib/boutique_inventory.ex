a7afea05eb329fa065a30aed6f7703a94e878d9f
Edward Ruzek <edward.ruzek@gmail.com>
2024-11-21 22:46:12 -0500

    completed boutique-inventory exercise for elixir

--- elixir/boutique-inventory/lib/boutique_inventory.ex
+++ elixir/boutique-inventory/lib/boutique_inventory.ex
@@ -1,21 +1,45 @@
 defmodule BoutiqueInventory do
   def sort_by_price(inventory) do
-    # Please implement the sort_by_price/1 function
+    inventory 
+    |> Enum.sort(&(&1[:price] <= &2[:price]))
   end
 
   def with_missing_price(inventory) do
-    # Please implement the with_missing_price/1 function
+    inventory
+    |> Enum.reduce([], fn item, acc -> 
+      case item[:price] do
+        nil -> acc ++ [Enum.into(item, %{})]
+        _ -> acc
+      end
+    end)
   end
 
   def update_names(inventory, old_word, new_word) do
-    # Please implement the update_names/3 function
+    inventory
+    |> Enum.reduce([], fn item, acc ->
+      m = Enum.map(item, fn {k, v} ->
+        case k do
+          :name -> {k, v |> String.replace(old_word, new_word)}
+          _ -> {k, v}
+        end
+      end)
+      [Enum.into(m, %{}) | acc]
+    end)
+    |> Enum.reverse
   end
 
   def increase_quantity(item, count) do
-    # Please implement the increase_quantity/2 function
+    q = item
+    |> Map.get(:quantity_by_size)
+    |> Enum.map(fn {k, v} -> {k, v+count} end)
+    |> Enum.into(%{})
+
+    Map.put(item, :quantity_by_size, q)
   end
 
   def total_quantity(item) do
-    # Please implement the total_quantity/1 function
+    item
+    |> Map.get(:quantity_by_size)
+    |> Enum.reduce(0, fn {_, v}, acc -> acc+v end)
   end
 end
