016293dbfede89fe36be4e9f90319435568cd887
Edward Ruzek <edward.ruzek@gmail.com>
2024-11-23 08:19:53 -0500

    completed newsletter exercise for elixir

--- elixir/newsletter/lib/newsletter.ex
+++ elixir/newsletter/lib/newsletter.ex
@@ -1,21 +1,39 @@
 defmodule Newsletter do
   def read_emails(path) do
-    # Please implement the read_emails/1 function
+    path |> File.read! |> String.split("\n", trim: true)
   end
 
   def open_log(path) do
-    # Please implement the open_log/1 function
+    File.open!(path, [:write])
   end
 
   def log_sent_email(pid, email) do
-    # Please implement the log_sent_email/2 function
+    pid |> IO.write(
+        email <> "\n"
+      )
   end
 
   def close_log(pid) do
-    # Please implement the close_log/1 function
+    File.close(pid)
   end
 
   def send_newsletter(emails_path, log_path, send_fun) do
-    # Please implement the send_newsletter/3 function
+    emails = emails_path |> read_emails
+  
+    log_pid = log_path |> open_log
+    
+    emails |> send_newsletter_to_email(log_pid, send_fun)
+
+    log_pid |> close_log
+  end
+  
+  defp send_newsletter_to_email([], _, _), do: nil
+  defp send_newsletter_to_email([email | tail], pid, send) do
+    sent? = send.(email)
+    case sent? do
+      :ok -> pid |> log_sent_email(email)
+      _ -> nil
+    end
+    tail |> send_newsletter_to_email(pid, send)
   end
 end
