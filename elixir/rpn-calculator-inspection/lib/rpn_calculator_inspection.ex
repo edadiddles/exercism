3fc8ba68f88d3bd70a091761c55b0f15e3e789c8
Edward Ruzek <edward.ruzek@gmail.com>
2024-11-29 07:15:36 -0500

    fixed issue suggested by elixir analyzer

--- elixir/rpn-calculator-inspection/lib/rpn_calculator_inspection.ex
+++ elixir/rpn-calculator-inspection/lib/rpn_calculator_inspection.ex
@@ -17,7 +17,7 @@ defmodule RPNCalculatorInspection do
   def reliability_check(calculator, inputs) do
     orig_trap_exit = Process.flag(:trap_exit, true)
     reliability_checks = Enum.reduce(inputs, [], fn input, acc ->
-      [start_reliability_check(calculator, input)] ++ acc
+      [start_reliability_check(calculator, input) | acc]
     end)
 
     results = Enum.reduce(reliability_checks, %{}, fn reliability_check, acc -> await_reliability_check_result(reliability_check, acc) end)
@@ -26,13 +26,12 @@ defmodule RPNCalculatorInspection do
   end
 
   def correctness_check(calculator, inputs) do
-    orig_trap_exit = Process.flag(:trap_exit, true)
     tasks = Enum.reduce(inputs, [], fn input, acc ->
-      [Task.async(fn -> calculator.(input) end)] ++ acc
+      [Task.async(fn -> calculator.(input) end) | acc]
     end)
 
     Enum.reduce(tasks, [], fn task, acc ->
-      [Task.await(task, 100)] ++ acc
+      [Task.await(task, 100) | acc]
     end)
   end
 end
