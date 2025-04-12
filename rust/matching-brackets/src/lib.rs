6332ac529d58cfc8177340aaf728c9f410a56308
Edward Ruzek <edadiddles@hotmail.com>
2025-04-12 10:43:25 -0400

    completed matching brackets exercise for rust

---
+++ rust/matching-brackets/src/lib.rs
@@ -0,0 +1,34 @@
+pub fn brackets_are_balanced(string: &str) -> bool {
+    let mut open_brackets = Vec::new();
+    for c in string.chars().collect::<Vec<char>>() {
+        let mut is_o_bracket = false;
+        let mut is_c_bracket = false;
+
+        if matches!(c, '[' | '{' | '(') {
+            is_o_bracket=true;
+        } else if matches!(c,  ']' | '}' | ')') {
+            is_c_bracket=true;
+        } else {
+            continue;
+        }
+
+        if is_o_bracket {
+            open_brackets.push(c);
+        } else if is_c_bracket {
+            if open_brackets.len() == 0 {
+                return false
+            }
+
+            let o_bracket = open_brackets.pop().unwrap();
+            if o_bracket == '{' && c != '}' {
+                return false
+            } else if o_bracket == '[' && c != ']' {
+                return false
+            } else if o_bracket == '(' && c != ')' {
+                return false
+            }
+        }
+    }
+
+    open_brackets.len() == 0
+}
