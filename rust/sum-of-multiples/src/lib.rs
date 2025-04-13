b8bd2a02d6d774c2cb92ad18b800558fbe6f73b6
Edward Ruzek <edadiddles@hotmail.com>
2025-04-13 08:17:18 -0400

    completed sum of multiples exercise for rust

---
+++ rust/sum-of-multiples/src/lib.rs
@@ -0,0 +1,18 @@
+pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
+    let mut vals: Vec<u32> = Vec::new();
+    for i in 0..factors.len() {
+        if factors[i] == 0 {
+            continue;
+        }
+        let mut j = 1;
+        while factors[i]*j < limit {
+            let mul = factors[i]*j;
+            if !vals.contains(&mul) {
+                vals.push(mul);
+            }
+            j += 1;
+        }
+    }
+
+    vals.iter().sum()
+}
