08d40156882756e4d54af602fbf9843043630177
Edward Ruzek <edadiddles@hotmail.com>
2025-04-13 08:19:27 -0400

    ran rust formatter to get trivial commit to resubmit due to Cargo.lock missing error

--- rust/sum-of-multiples/src/lib.rs
+++ rust/sum-of-multiples/src/lib.rs
@@ -5,8 +5,8 @@ pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
             continue;
         }
         let mut j = 1;
-        while factors[i]*j < limit {
-            let mul = factors[i]*j;
+        while factors[i] * j < limit {
+            let mul = factors[i] * j;
             if !vals.contains(&mul) {
                 vals.push(mul);
             }
