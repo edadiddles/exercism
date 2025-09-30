c4a03b25c849d4d4ea931dd3a5c1d4d0ee47eb12
Edward Ruzek <edadiddles@hotmail.com>
2025-09-30 08:44:15 -0400

    solution to collatz conjecture exercise for python

---
+++ python/collatz-conjecture/collatz_conjecture.py
@@ -0,0 +1,11 @@
+def steps(number):
+    if number <= 0:
+        raise ValueError("Only positive integers are allowed")
+
+    num_steps = 0
+
+    while number > 1:
+        num_steps += 1
+        number = number / 2 if number % 2 == 0 else (3 * number + 1)
+
+    return num_steps
