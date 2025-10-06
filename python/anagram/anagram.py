0df3e8f8a7e1642b0451d50b7d8cf38834b1705e
Edward Ruzek <edadiddles@hotmail.com>
2025-10-06 08:16:17 -0400

    solution to anagram exercise for python

---
+++ python/anagram/anagram.py
@@ -0,0 +1,7 @@
+def find_anagrams(word, candidates):
+    anagrams = []
+    for candidate in candidates:
+        if word.lower() != candidate.lower() and sorted(word.lower()) == sorted(candidate.lower()):
+            anagrams.append(candidate)
+
+    return anagrams
