bdb1245fa7ea46455e4dceb17686b9065b7c04b5
Edward Ruzek <edadiddles@hotmail.com>
2025-10-15 08:54:40 -0400

    fixing use of round function

--- python/space-age/space_age.py
+++ python/space-age/space_age.py
@@ -5,25 +5,25 @@ class SpaceAge:
         self.years = seconds/31557600
 
     def on_earth(self):
-        return round(100*self.years)/100
+        return round(self.years, 2)
 
     def on_mercury(self):
-        return round(100*self.years/0.2408467)/100
+        return round(self.years/0.2408467, 2)
 
     def on_venus(self):
-        return round(100*self.years/0.61519726)/100
+        return round(self.years/0.61519726, 2)
 
     def on_mars(self):
-        return round(100*self.years/1.8808158)/100
+        return round(self.years/1.8808158, 2)
 
     def on_jupiter(self):
-        return round(100*self.years/11.862615)/100
+        return round(self.years/11.862615, 2)
 
     def on_saturn(self):
-        return round(100*self.years/29.447498)/100
+        return round(self.years/29.447498, 2)
 
     def on_uranus(self):
-        return round(100*self.years/84.016846)/100
+        return round(self.years/84.016846, 2)
 
     def on_neptune(self):
-        return round(100*self.years/164.79132)/100
+        return round(self.years/164.79132, 2)
