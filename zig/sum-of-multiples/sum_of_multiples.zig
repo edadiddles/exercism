1421c215f764fe511d4f70baefd79b8491d63159
Edward Ruzek <edadiddles@hotmail.com>
2025-11-05 08:45:04 -0500

    improving performance and hopefully space efficiency

--- zig/sum-of-multiples/sum_of_multiples.zig
+++ zig/sum-of-multiples/sum_of_multiples.zig
@@ -2,16 +2,19 @@ const std = @import("std");
 const mem = std.mem;
 
 pub fn sum(allocator: mem.Allocator, factors: []const u32, limit: u32) !u64 {
-    var multiples = try allocator.alloc(u64, limit);
+    var multiples = try allocator.alloc(u8, limit);
     defer allocator.free(multiples);
 
     var energy_points: u64 = 0;
-    for (1..limit) |k| {
-        for (factors) |f| {
-            if (f != 0 and k % f == 0 and multiples[k-1] != 1) {
+    for (factors) |f| {
+        if (f == 0) { continue; }
+        var k = f;
+        while (k < limit) { 
+            if (multiples[k-1] != 1) {
                 energy_points += k;
                 multiples[k-1] = 1;
             }
+            k += f;
         }
     }
 
