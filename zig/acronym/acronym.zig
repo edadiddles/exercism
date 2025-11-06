4d2049712fa67a706d9fbf7ab97f1850849d199c
Edward Ruzek <edadiddles@hotmail.com>
2025-11-06 09:11:21 -0500

    solution to acronym exercise for zig. Had to check solns to understand how to allocate memory properly

---
+++ zig/acronym/acronym.zig
@@ -0,0 +1,22 @@
+const std = @import("std");
+const mem = std.mem;
+
+pub fn abbreviate(allocator: mem.Allocator, words: []const u8) mem.Allocator.Error![]u8 {
+    var acronym = std.ArrayList(u8).init(allocator);
+    if (words.len == 0) {
+        return acronym.toOwnedSlice();
+    }
+
+    var isNewWord = true;
+    for (words) |c| {
+        if (isNewWord and std.ascii.isAlphabetic(c)) {
+            try acronym.append(std.ascii.toUpper(c));
+            isNewWord = false;
+        }
+        if (c == ' ' or c == '-') {
+            isNewWord = true;
+        }
+    }
+
+    return acronym.toOwnedSlice();
+}
