87cc1410d358b3143f789c6293a82fa45801ea2f
Edward Ruzek <edadiddles@hotmail.com>
2025-11-07 08:40:47 -0500

    updated to zig 0.15.2 and modified code to compile and pass tests locally

--- zig/acronym/acronym.zig
+++ zig/acronym/acronym.zig
@@ -2,15 +2,16 @@ const std = @import("std");
 const mem = std.mem;
 
 pub fn abbreviate(allocator: mem.Allocator, words: []const u8) mem.Allocator.Error![]u8 {
-    var acronym = std.ArrayList(u8).init(allocator);
+    var acronym: std.ArrayList(u8) = .empty;
+
     if (words.len == 0) {
-        return acronym.toOwnedSlice();
+        return acronym.toOwnedSlice(allocator);
     }
 
     var isNewWord = true;
     for (words) |c| {
         if (isNewWord and std.ascii.isAlphabetic(c)) {
-            try acronym.append(std.ascii.toUpper(c));
+            try acronym.append(allocator, std.ascii.toUpper(c));
             isNewWord = false;
         }
         if (c == ' ' or c == '-') {
@@ -18,5 +19,5 @@ pub fn abbreviate(allocator: mem.Allocator, words: []const u8) mem.Allocator.Err
         }
     }
 
-    return acronym.toOwnedSlice();
+    return acronym.toOwnedSlice(allocator);
 }
