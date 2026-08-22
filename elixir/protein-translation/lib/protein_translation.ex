0878a6e3a0c38e0f8755c43c6c6689dfb25034c6
Edward Ruzek <edadiddles@hotmail.com>
2026-08-22 09:25:55 -0400

    solution to protein-translation for elixir

---
+++ elixir/protein-translation/lib/protein_translation.ex
@@ -0,0 +1,69 @@
+defmodule ProteinTranslation do
+  @doc """
+  Given an RNA string, return a list of proteins specified by codons, in order.
+  """
+  @spec of_rna(String.t()) :: {:ok, list(String.t())} | {:error, String.t()}
+  def of_rna(rna) do
+    case r_parse_rna(rna, []) do
+      {:ok, codons} -> {:ok, codons}
+      {:error, _} -> {:error, "invalid RNA"}
+    end
+  end
+
+  defp r_parse_rna("", acc) do
+    {:ok, acc}
+  end
+
+  defp r_parse_rna(rna, acc) do
+    case rna |> String.slice(0..2) |> of_codon do
+      {:ok, "STOP"} -> {:ok, acc}
+      {:ok, codon} -> r_parse_rna(rna |> String.slice(3..-1//1), acc ++ [codon])
+      {:error, err} -> {:error, err}
+    end 
+  end
+
+  @doc """
+  Given a codon, return the corresponding protein
+
+  UGU -> Cysteine
+  UGC -> Cysteine
+  UUA -> Leucine
+  UUG -> Leucine
+  AUG -> Methionine
+  UUU -> Phenylalanine
+  UUC -> Phenylalanine
+  UCU -> Serine
+  UCC -> Serine
+  UCA -> Serine
+  UCG -> Serine
+  UGG -> Tryptophan
+  UAU -> Tyrosine
+  UAC -> Tyrosine
+  UAA -> STOP
+  UAG -> STOP
+  UGA -> STOP
+  """
+  @spec of_codon(String.t()) :: {:ok, String.t()} | {:error, String.t()}
+  def of_codon(codon) do
+    case codon do
+      "UGU" -> {:ok, "Cysteine"}
+      "UGC" -> {:ok, "Cysteine"}
+      "UUA" -> {:ok, "Leucine"}
+      "UUG" -> {:ok, "Leucine"}
+      "AUG" -> {:ok, "Methionine"}
+      "UUU" -> {:ok, "Phenylalanine"}
+      "UUC" -> {:ok, "Phenylalanine"}
+      "UCU" -> {:ok, "Serine"}
+      "UCC" -> {:ok, "Serine"}
+      "UCA" -> {:ok, "Serine"}
+      "UCG" -> {:ok, "Serine"}
+      "UGG" -> {:ok, "Tryptophan"}
+      "UAU" -> {:ok, "Tyrosine"}
+      "UAC" -> {:ok, "Tyrosine"}
+      "UAA" -> {:ok, "STOP"}
+      "UAG" -> {:ok, "STOP"}
+      "UGA" -> {:ok, "STOP"}
+      _ -> {:error, "invalid codon"}
+    end
+  end
+end
